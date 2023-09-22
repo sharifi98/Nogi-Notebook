40928343a02de498f55d5d1afb90b02f15b160fe
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-09-22 13:36:51 +0200

    Initial Commit

---
+++ Nogi Notebook/Nogi_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nogi_NotebookApp.swift
+//  Nogi Notebook
+//
+//  Created by Hossein Sharifi on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nogi_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
